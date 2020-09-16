ee83385340f805bd309fff24b7eeb0bd2af702e4
junmin-Chang <chjm219@kookmin.ac.kr>
2020-09-17 00:45:55 +0900

    어휘 자동업데이트 -> 업데이트 버튼 삭제

--- app/src/main/java/doubleslash05/mini/team11/MainActivity.java
+++ app/src/main/java/doubleslash05/mini/team11/MainActivity.java
@@ -30,16 +30,12 @@ import edu.cmu.pocketsphinx.Config;
 
 public class MainActivity extends AppCompatActivity implements RapidSphinxListener {
 
-    public static final int sub = 1001;
 
-    // 이제 응용해봐야하는데..
 
     private RapidSphinx rapidSphinx;
     private Button btnRecognizer;
 
 
-    //    private Button btnStartAudio; 오디오 출력 필요없음.
-    private Button btnSync;
     private EditText txtWords;
     private EditText txtDistractor;
     private TextView txtResult;
@@ -100,6 +96,7 @@ public class MainActivity extends AppCompatActivity implements RapidSphinxListen
 
 
 
+
         }
 
         if (txtResult.getText().toString().equals(nextKeyword)) {
@@ -131,7 +128,6 @@ public class MainActivity extends AppCompatActivity implements RapidSphinxListen
         }
 
         // 지웑되지 않는 언어에 대한 로그 처리 한국어는 되는지 테스트 후 확인해봐야겠음..  -> 확인결과 한국어는 지원이 안됨.
-        // -> 하지만 updateVocabulary 메소드에 감지하고 싶은 텍스트를 때려 넣으면 가능 할 것 같음.
         txtUnsupported.setText("지원되지 않는 언어 : \n" + unsupportedWords);
     }
 
@@ -171,7 +167,6 @@ public class MainActivity extends AppCompatActivity implements RapidSphinxListen
             @Override
             public void rapidPostExecute(boolean isSuccess) {
 
-                btnSync.setEnabled(true);
                 btnRecognizer.setEnabled(false);
                 txtStatus.setText("RapidSphinx ready!");
                 dialog.dismiss();
@@ -182,38 +177,41 @@ public class MainActivity extends AppCompatActivity implements RapidSphinxListen
     }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-
-
-
-
-        // onCreate 시에 어휘 업데이트 (버튼 누를 필요 없이 )
+    protected void onResume() {
+        super.onResume();
 
 
         dialog.show();
-        btnSync.setEnabled(false);
         btnRecognizer.setEnabled(true);
 
         String[] oovwords = {"pause"};
 
-        rapidSphinx.updateVocabulary("next",
+
+
+        // 어휘 자동 업뎃!
+        rapidSphinx.updateVocabulary(nextKeyword,
                 oovwords, new RapidCompletionListener() {
                     @Override
                     public void rapidCompletedProcess() {
 
 
+
+
                         txtResult.setText("");
                         txtPartialResult.setText("");
-                        txtStatus.setText("");
+                        txtStatus.setText("어휘 자동 업데이트됨!");
                         txtUnsupported.setText("");
                         btnRecognizer.setEnabled(true);
                         dialog.dismiss();
+
+
                     }
                 });
 
+    }
 
-
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
@@ -234,7 +232,6 @@ public class MainActivity extends AppCompatActivity implements RapidSphinxListen
         txtPartialResult = findViewById(R.id.txtPartialResult);
         txtUnsupported = findViewById(R.id.txtUnsuported);
         txtStatus = findViewById(R.id.txtStatus);
-        btnSync = findViewById(R.id.btnSync);
         btnRecognizer = findViewById(R.id.btnRecognizer);
         btnStop = findViewById(R.id.btnStop);
 
@@ -242,45 +239,12 @@ public class MainActivity extends AppCompatActivity implements RapidSphinxListen
         txtStatus.setText("데이터를 준비하는 중 입니다!");
 
         // 실행 초기 버튼 비활성화
-        btnSync.setEnabled(false);
         btnRecognizer.setEnabled(false);
 
 
-        // 싱크 버튼을 누를때 어휘를 업데이트한다.
-//        btnSync.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//
-//
-//                dialog.show();
-//                btnSync.setEnabled(false);
-//                btnRecognizer.setEnabled(true);
-//
-//                String[] oovwords = {"pause"};
-//
-//                rapidSphinx.updateVocabulary("next",
-//                        oovwords, new RapidCompletionListener() {
-//                            @Override
-//                            public void rapidCompletedProcess() {
-//
-//
-//                                txtResult.setText("");
-//                                txtPartialResult.setText("");
-//                                txtStatus.setText("");
-//                                txtUnsupported.setText("");
-//                                btnRecognizer.setEnabled(true);
-//                                dialog.dismiss();
-//                            }
-//                        });
-//
-//
-//
-//            }
-//        });
-
 
         // 인식 시작 버튼 --> 버튼을 안누르고 어떻게 해봐야 할 것 같음. 레시피 클릭시 싱크 버튼을 눌렀다고 치면, 그 후에 자동으로 btnRecognizer 실행 하게끔.
+        // 메뉴 카테고리 클릭
         btnRecognizer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -288,7 +252,6 @@ public class MainActivity extends AppCompatActivity implements RapidSphinxListen
                 txtPartialResult.setText("");
                 txtStatus.setText("");
                 txtUnsupported.setText("");
-                btnSync.setEnabled(false);
                 btnRecognizer.setEnabled(true);
                 rapidSphinx.startRapidSphinx(10000);
                 txtStatus.setText("지금 말하세요!");
@@ -325,7 +288,6 @@ public class MainActivity extends AppCompatActivity implements RapidSphinxListen
                 @Override
                 public void rapidPostExecute(boolean isSuccess) {
 
-                    btnSync.setEnabled(true);
                     btnRecognizer.setEnabled(true);
                     txtStatus.setText("RapidSphinx 준비됨!");
                     dialog.dismiss();
