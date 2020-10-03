566fa5b8148c9db66bef90fff6874a57d4a4f5f1
junmin-Chang <chjm219@kookmin.ac.kr>
2020-10-03 21:09:39 +0900

    font change

--- app/src/main/java/doubleslash05/mini/team11/ui/tutorial/Tutorial1Fragment.kt
+++ app/src/main/java/doubleslash05/mini/team11/ui/tutorial/Tutorial1Fragment.kt
@@ -1,6 +1,8 @@
 package doubleslash05.mini.team11.ui.tutorial
 
+import android.content.res.AssetManager
 import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.text.Html
 import android.text.Spannable
@@ -16,6 +18,7 @@ import doubleslash05.mini.team11.ui.base.BaseFragment
 import kotlinx.android.synthetic.main.fragment_tutorial_page1.*
 
 class Tutorial1Fragment : BaseFragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,6 +34,7 @@ class Tutorial1Fragment : BaseFragment() {
         tvGuide1.text = "stop"
         tvLabel1.setText(Html.fromHtml("<br>안녕하세요!</br><br>이제부터<font color='#FF9E00'>쿠키스</font>와 함께</br><br><font color='#FF9E00'>목소리</font>로 요리해보아요.</br>"))
         tvDetail1.setText(Html.fromHtml("레시피 영상 재생시 <font color='#FF9E00'>이전</font> 이라고 말하면<br><font color='#FF9E00'>이전</font> 스텝이 시작됩니다.</br>"))
+
     }
 
 }
