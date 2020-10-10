44e79a0444906fc35bab8725f0b4fdca32289fe7
양석진 <sjyang960@gmail.com>
2020-10-11 04:57:46 +0900

    SearchView 추가

---
+++ app/src/main/java/doubleslash05/mini/team11/ui/search/SearchActivity.java
@@ -0,0 +1,66 @@
+package doubleslash05.mini.team11.ui.search;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import doubleslash05.mini.team11.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    MenuItem mSearch;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        mSearch = menu.findItem(R.id.search);
+
+
+        SearchView sv = (SearchView) mSearch.getActionView();
+        sv.setQueryHint("재료명,메뉴명");
+        sv.setMaxWidth(Integer.MAX_VALUE);
+
+
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+
+}
