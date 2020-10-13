60fea8e55c53a3dbb055a4cba7fdb39bd78cc091
HoYean Lee <ghdusjang8@gmail.com>
2020-10-13 15:58:44 +0900

    Splash 처리

---
+++ app/src/main/java/doubleslash05/mini/team11/ui/splash/SplashActivity.kt
@@ -0,0 +1,48 @@
+package doubleslash05.mini.team11.ui.splash
+
+import android.animation.Animator
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import doubleslash05.mini.team11.R
+import doubleslash05.mini.team11.ui.tutorial.TutorialActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        animationview_splash.addAnimatorListener(object : Animator.AnimatorListener{
+            override fun onAnimationRepeat(animation: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                val intent = Intent(application, TutorialActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+
+            }
+        })
+
+//        animationview_splash.setAnimation("lottie_splash.json")
+//        animationview_splash.playAnimation()
+
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val intent = Intent(application, TutorialActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 3000)
+    }
+
+
+}
